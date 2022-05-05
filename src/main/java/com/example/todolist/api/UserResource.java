a4c0320a022715d56bb285aecc6fcfb774bcad50
Dat Nguyen <datns1610@gmail.com>
2022-05-05 10:50:06 +0700

    move email service

--- src/main/java/com/example/todolist/api/UserResource.java
+++ src/main/java/com/example/todolist/api/UserResource.java
@@ -10,7 +10,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.example.todolist.email.MailSender;
+import com.example.todolist.service.MailSender;
 import com.example.todolist.entity.User;
 import com.example.todolist.entity.VerificationOtp;
 import com.example.todolist.service.UserService;
